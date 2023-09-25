0ff708d5bdaa999c7137f3d3cfdeca22c5f31358
Ruslan <xeont88@gmail.com>
2023-09-25 21:27:43 +0300

    Добавлено: клавиатура, админ панель, приветствие стикером

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 
@@ -6,10 +7,53 @@ load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot=bot)
 
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('Каталог').add('Корзина').add('Контакты')
+
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Каталог').add('Корзина').add('Контакты').add('Админ панель')
+
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить товар').add('Удалить товар').add('Сделать рассылку')
+
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer(f'{message.from_user.first_name}, добро пожаловать в бот для приема заявок!')
+    await message.answer_sticker('CAACAgIAAxkBAAIB62URx90LvAIrSM6ystcub2xoULrPAAIFAAPANk8T-WpfmoJrTXUwBA')
+    await message.answer(f'{message.from_user.first_name}, добро пожаловать в бот для приема заявок!',
+                         reply_markup=main)
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'Привет админ!', reply_markup=main_admin)
+
+
+@dp.message_handler(text='Корзина')
+async def cart(message: types.Message):
+    await message.answer(f'Корзина пуста')
+
+
+@dp.message_handler(text='id')
+async def cart(message: types.Message):
+    await message.answer(f'{message.from_user.id}')
+
+
+@dp.message_handler(text='Админ панель')
+async def cart(message: types.Message):
+    if message.from_user.id == int(os.getenv('ADMIN_ID')):
+        await message.answer(f'АД-минка!', reply_markup=admin_panel)
+    else:
+        await message.reply('Я тебя не понимаю ...')
+
+
+# @dp.message_handler(content_types=['sticker'])
+# async def check_sticker(message: types.Message):
+#     await message.answer(message.sticker.file_id)
+#     await bot.send_message(message.from_user.id, message.chat.id)
+
+
+# @dp.message_handler(content_types=['document', 'photo'])
+# async def forward_message(message: types.Message):
+#     await bot.forward_message(os.getenv('GROUP_ID'), message.from_user.id, message.message_id)
 
 
 @dp.message_handler()
