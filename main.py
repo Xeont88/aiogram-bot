27af98c5acba6b5dedad6b500e137c6251fac924
Ruslan <xeont88@gmail.com>
2023-09-25 20:41:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types, executor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, добро пожаловать в бот для приема заявок!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Я тебя не понимаю ...')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
